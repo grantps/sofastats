5da512aac4a9f91ae3aa9a3df63970f6e4386f45
Grant Paton-Simpson <grant@p-s.co.nz>
2025-10-14 16:42:06 +1300

    Add diagnostic information

--- src/sofastats/ui/panel_server.py
+++ src/sofastats/ui/panel_server.py
@@ -6,7 +6,13 @@ from webbrowser import open_new_tab
 
 def serve():
     cwd = Path(__file__).parent
-    os.chdir(cwd)
+    try:
+        os.chdir(cwd)
+    except FileNotFoundError as e:
+        print(f"Can't change directory to '{cwd}' for some reason. Orig error: {e}")
     Popen(f"panel serve ui.py", shell=True)
-    sleep(2)
+    sleep(2)  ## to give server time to be ready before tab opens
     open_new_tab("http://localhost:5006/ui")
+
+if __name__ == '__main__':
+    serve()
