6fa532ff5435aca68b82fa0c6fde5e8cbedd3b11
Grant Paton-Simpson <grant@p-s.co.nz>
2025-10-27 23:17:49 +1300

    Add nice and positioned HTML tool tips

--- src/sofastats/ui/stats/stats_tab.py
+++ src/sofastats/ui/stats/stats_tab.py
@@ -4,6 +4,8 @@ Stats form, stats chooser, and stats config
 Using nested modals.
 Upper modals and buttons opening and closing them must be defined inside lower modals.
 """
+from bokeh.models import Tooltip
+from bokeh.models.dom import HTML
 import panel as pn
 
 from sofastats.ui.conf import Colour, StatsOption
@@ -11,6 +13,33 @@ from sofastats.ui.stats.stats_chooser import get_stats_chooser_modal
 from sofastats.ui.stats.stats_config import get_stats_config_modal
 
 pn.extension('modal')
+
+css = """
+.bk-panel-models-widgets-TooltipIcon {
+    padding: 4px 3px 4px 0;
+    margin: 0 10px 0 -10px;
+    background-color: #bbbbbb;
+    border-radius: 0 12px 12px 0;
+}
+"""
+pn.extension(raw_css=[css])
+
+def get_html_tooltip(
+        html_content: str, *, width: int, horizontal_offset: int, vertical_offset: int, show_arrow=False) -> Tooltip:
+    """
+    offsets: WARNING - if horizontal_offset and vertical_offset result in the tooltip overlapping the button
+      then you will never see it because the mouse being over the top makes it go away.
+      No defaults supplied because it is a custom question depending on width and height of button.
+    """
+    tooltip = Tooltip(
+        content=HTML(html=html_content),
+        position=(horizontal_offset, vertical_offset),
+        attachment='below',  ## TooltipAttachment enum has values
+        show_arrow=show_arrow,
+        styles={'width': f'{width}px'},
+        interactive=False, closable=False,
+    )
+    return tooltip
 
 def get_stats_main():
     servables = pn.Column()
@@ -40,20 +69,57 @@ def get_stats_main():
     btn_open_stats_chooser.on_click(open_stats_chooser)
     get_help_row = pn.Row(stats_text, btn_open_stats_chooser, styles=stats_need_help_style, width=800)
 
+    STATS_BTN_WIDTH = 350
+    STATS_BTN_HEIGHT = 30
+
     stats_btn_kwargs = {
         'button_type': 'primary',
-        'width': 350,
+        'width': STATS_BTN_WIDTH,
+        'height': STATS_BTN_HEIGHT,
     }
-    btn_anova = pn.widgets.Button(name='ANOVA', description="ANOVA", **stats_btn_kwargs)
+
+    tool_tip_icon_kwargs = {
+        'width': STATS_BTN_WIDTH - 15,
+        'horizontal_offset': -180,
+    }
+
+    anova_html = """\
+    <h1>ANOVA (Analysis Of Variance)</h1>
+    <p>The ANOVA (Analysis Of Variance) is good for seeing if there is a difference in means between multiple groups
+    when the data is numerical and adequately normal. Generally the ANOVA is robust to non-normality.</p>
+    <p>You can evaluate normality by clicking on the "Check Normality" button (under construction).</p>
+    <p>The Kruskal-Wallis H may be preferable if your data is not adequately normal.</p>
+    <img src="images/bunny_head_small.svg" alt="Bunny logo" width=50px></img>"""
+    chi_square_html = """\
+    <h1>Chi Square Test</h1>
+    <p>The Chi Square test is one of the most widely used tests in social science.
+    It is good for seeing if the results for two variables are independent or related.
+    Is there a relationship between gender and income group for example?</p>
+    <img src="images/bunny_head_small.svg" alt="Bunny logo" width=50px></img
+    """
+    under_construction_html = """\
+    <h1>Under Construction</h1>
+    """
+    btn_anova = pn.widgets.Button(name='ANOVA', **stats_btn_kwargs)
+    anova_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(anova_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_chi_square = pn.widgets.Button(name='Chi Square', description='Chi Square', **stats_btn_kwargs)
+    chi_square_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(chi_square_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_indep_ttest = pn.widgets.Button(name='Independent Samples T-Test', description='Independent Samples T-Test', **stats_btn_kwargs)
+    indep_ttest_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_kruskal_wallis = pn.widgets.Button(name='Kruskal-Wallis H', description='Kruskal-Wallis H', **stats_btn_kwargs)
+    kruskal_wallis_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_mann_whitney = pn.widgets.Button(name='Mann-Whitney U', description='Mann-Whitney U', **stats_btn_kwargs)
+    mann_whitney_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_normality = pn.widgets.Button(name='Normality', description='Normality', **stats_btn_kwargs)
+    normality_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_paired_ttest = pn.widgets.Button(name='Paired Samples T-Test', description='Paired Samples T-Test', **stats_btn_kwargs)
+    paired_ttest_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_pearsons = pn.widgets.Button(name="Pearson's R Correlation", description="Pearson's R Correlation", **stats_btn_kwargs)
+    pearsons_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_spearmans = pn.widgets.Button(name="Spearman's R Correlation", description="Spearman's R Correlation", **stats_btn_kwargs)
+    spearmans_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_wilcoxon = pn.widgets.Button(name='Wilcoxon Signed Ranks', description='Wilcoxon Signed Ranks', **stats_btn_kwargs)
+    wilcoxon_tip = pn.widgets.TooltipIcon(value=get_html_tooltip(under_construction_html, vertical_offset=-20, **tool_tip_icon_kwargs))
     btn_close = pn.widgets.Button(name="Close")
     def open_anova_config(_event):
         stats_config_modal = get_stats_config_modal(StatsOption.ANOVA, btn_close)
@@ -82,8 +148,20 @@ def get_stats_main():
         pn.pane.Markdown(
             "### Select the type of test you want to run", styles={'color': Colour.BLUE_MID, 'font-size': '16px'}),
         pn.Row(
-            pn.Column(btn_anova, btn_chi_square, btn_indep_ttest, btn_kruskal_wallis, btn_mann_whitney),
-            pn.Column(btn_normality, btn_paired_ttest, btn_pearsons, btn_spearmans, btn_wilcoxon),
+            pn.Column(
+                pn.Row(btn_anova, anova_tip),
+                pn.Row(btn_chi_square, chi_square_tip),
+                pn.Row(btn_indep_ttest, indep_ttest_tip),
+                pn.Row(btn_kruskal_wallis, kruskal_wallis_tip),
+                pn.Row(btn_mann_whitney, mann_whitney_tip),
+            ),
+            pn.Column(
+                pn.Row(btn_normality, normality_tip),
+                pn.Row(btn_paired_ttest, paired_ttest_tip),
+                pn.Row(btn_pearsons, pearsons_tip),
+                pn.Row(btn_spearmans, spearmans_tip),
+                pn.Row(btn_wilcoxon, wilcoxon_tip),
+            ),
         ),
         servables,
     )
