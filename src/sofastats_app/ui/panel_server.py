7d526bd874d8ef6a9ac193bd3bfc0521fffa6589
Grant Paton-Simpson <grant@p-s.co.nz>
2025-11-14 06:07:39 +1300

    Change approach to running server:
    * Use thread for long-running server process
    * Show output of server thread in stdout
    * Add friendly messages to user
    * Terminate server process when overall process terminated

--- src/sofastats_app/ui/panel_server.py
+++ src/sofastats_app/ui/panel_server.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-from subprocess import Popen
+import subprocess
+import threading
 from time import sleep
 from webbrowser import open_new_tab
 
@@ -10,10 +11,19 @@ def serve():
         os.chdir(cwd)
     except FileNotFoundError as e:
         print(f"Can't change directory to '{cwd}' for some reason. Orig error: {e}")
-    ## https://discourse.bokeh.org/t/protocol-error-token-is-expired/11575
-    Popen(f"panel serve ui.py --static-dirs images=./images --session-token-expiration=900000", shell=True)
+    def run_server():
+        args = f"panel serve ui.py --static-dirs images=./images --session-token-expiration=900000"  ## https://discourse.bokeh.org/t/protocol-error-token-is-expired/11575
+        subprocess.run(args, shell=True)
+    print("Hi there - just starting SOFA Stats")
+    print("Waiting a couple of seconds so everything's ready")
+    output_thread = threading.Thread(target=run_server)
+    output_thread.start()
     sleep(2)  ## to give server time to be ready before tab opens
     open_new_tab("http://localhost:5006/ui")
+    print("I just opened a new tab in your web browser with the SOFA Stats App - enjoy!")
+    print("Don't worry about all the technical chatter below - "
+        "it might become useful if SOFA Stats stops working properly for some reason.")
+    output_thread.join()
 
 if __name__ == '__main__':
     serve()
